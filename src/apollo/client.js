12804a8db75c32990a54f45bb18b7388d09852fb
gmx <www.runit@gmail.com>
2022-12-24 23:16:50 +0300

    new url ks

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,7 +1,7 @@
 import {ApolloClient, InMemoryCache} from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: 'http://localhost:4000/api/graphql',
+    uri: 'http://ovz1.j47206473.wmekm.vps.myjino.ru:49423/api/graphql',
     cache: new InMemoryCache(),
 })
 
