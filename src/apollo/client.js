cad0eaaf5c27275f67e9b36d9b470ebf68a67084
EugenyGorshkov <gorshkov.zhenya@mail.ru>
2022-12-27 12:24:30 +0300

    Api Url without port

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,7 +1,7 @@
 import {ApolloClient, InMemoryCache} from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: 'https://oeebg.space:49423/api/graphql',
+    uri: 'https://oeebg.space/api/graphql',
     cache: new InMemoryCache(),
 })
 
