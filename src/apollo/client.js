7eaef8ca51d2c3abd859267cf1cb689687af365a
Oleg <www.runit@gmail.com>
2022-12-28 16:06:36 +0300

    new api server

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,7 +1,7 @@
 import {ApolloClient, InMemoryCache} from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: 'http://65.21.56.211:4000/api/graphql',
+    uri: 'https://oeebg.space/api/graphql',
     cache: new InMemoryCache(),
 })
 
