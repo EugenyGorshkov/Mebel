6a2f0206c94fbd4ddf8898298cd66c0fb418e128
EugenyGorshkov <gorshkov.zhenya@mail.ru>
2022-12-30 16:37:19 +0300

    add functional to modal (arrow handler)

--- src/components/GalleryPage/GalleryModal/GalleryModal.jsx
+++ src/components/GalleryPage/GalleryModal/GalleryModal.jsx
@@ -2,20 +2,31 @@ import React, { useEffect, useState } from 'react';
 import cn from 'classnames'
 
 import crossX from '../../../assets/closeX.svg'
+import arrrowRight from '../../../assets/arrows/white-chevron-right.png'
+import arrrowLeft from '../../../assets/arrows/white-chevron-left.png'
 import { useQuery } from '@apollo/client';
 import { FETCH_IMAGES } from '../../../apollo/Gallery';
 import Loader from '../../UI/Loader/Loader';
 
+import styles from './GalleryModal.module.scss'
+
 const GalleryModal = ({
+    allImg,
     activeModalHandler,
     activeModal,
     currentImgId,
     currentImgUrl,
+    currentImgPosition,
+    setCurrentImgPostion,
+    setCurrentImgUrl,
 }) => {
-    const [allImg, setAllImg] = useState([])
+    //тут не используется пока что стейты все стейты из пропсов
+    const [disableLeftArrow, setDisaleLeftArrow] = useState(false);
+    const [disableRightArrow, setDisaleRightArrow] = useState(false);
 
     const { loading, error, data } = useQuery(FETCH_IMAGES)
 
+    // условные обработчики данных
     if (loading) {
         return (
             <div className={cn('absolute top-0 left-0 h-full w-full overflow-hidden', activeModal ? 'block' : 'hidden')}>
@@ -33,6 +44,37 @@ const GalleryModal = ({
             </div>
         )
     }
+    // функции обработчики событий 
+    const arrowRightHandler = () => {
+        let index = 0;
+        for(let element of allImg) {
+            console.log(element);
+            if (index === currentImgPosition) {
+                setCurrentImgPostion(index+1)
+                setCurrentImgUrl(allImg[index+1].content.publicUrl)
+                console.log(currentImgPosition);
+                break
+            }
+            index++
+        }
+    }
+    const arrowLeftHandler = () => {
+        let index = 0;
+        for(let element of allImg) {
+            if (index === currentImgPosition) {
+                console.log(currentImgPosition);
+                setCurrentImgPostion(index-1)
+                setCurrentImgUrl(allImg[index-1].content.publicUrl)
+                
+                break
+            }
+            index++
+        }
+    }
+    const scrollHandler = () => {
+
+    }
+
 
     return (
         <div className={cn('absolute top-0 left-0 h-full w-full overflow-hidden', activeModal ? 'block' : 'hidden')}>
@@ -47,8 +89,29 @@ const GalleryModal = ({
                 <img src={crossX} alt="" className='max-h-8'/>
             </div>
             {/* modal body */}
-            <div className={cn('z-50 absolute flex top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] max-h-[80%]', activeModal ? 'block' : 'hidden')}>
-                <div className='h-full'><img src={currentImgUrl} alt="" className='h-full'/></div>
+            <div className={cn('z-50 absolute flex top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] max-h-[80%]', activeModal ? 'block' : 'hidden',styles.modalBody)}>
+                <div className='h-full relative'>
+                    {/* Arrrow Left */}
+                    <img 
+                        src={arrrowLeft} 
+                        alt="" 
+                        className={cn('absolute top-[50%] left-0 translate-y-[-50%]', styles.arrows)}
+                        onClick={arrowLeftHandler} 
+                    />
+                    {/* Main Pic */}
+                    <img 
+                        src={currentImgUrl} 
+                        alt="" 
+                        className='h-full'
+                    />
+                    {/* Arrrow Left */}
+                    <img 
+                        src={arrrowRight} 
+                        alt="" 
+                        className={cn('absolute top-[50%] right-0 translate-y-[-50%]', styles.arrows)}
+                        onClick={arrowRightHandler}
+                    />
+                </div>
             </div>
         </div>
     )
--- src/containers/GalleryPage/GalleryPage.jsx
+++ src/containers/GalleryPage/GalleryPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
 import { FETCH_IMAGES } from "../../apollo/Gallery.js";
 import Loader from "../../components/UI/Loader/Loader.jsx";
@@ -10,12 +10,15 @@ const GalleryPage = ({
     const [activeModal, setActiveModal] = useState(false);
     const [currentImgId, setCurrentImgId] = useState('')
     const [currentImgUrl, setCurrentImgUrl] = useState('')
+    const [currentImgPosition, setCurrentImgPostion] = useState(0)
 
-    const activeModalHandler = (id, url) => {
+    // открытие модалки и стартовые данные сразу уходят в модалку
+    const activeModalHandler = (id, url, index) => {
         activeModal ? setActiveModal(false) : setActiveModal(true)
         activeModal ? setScrollModal(false) : setScrollModal(true)
         setCurrentImgId(id)
         setCurrentImgUrl(url)
+        setCurrentImgPostion(index)
     }
 
     const { loading, error, data } = useQuery(FETCH_IMAGES)
@@ -43,18 +46,22 @@ const GalleryPage = ({
             <div className='container mx-auto'>
                 <h2 className="text-center p-5 font-bold text-xl">Примеры работ</h2>
                 <GalleryModal 
+                    allImg={data.galleries}
                     activeModalHandler={activeModalHandler} 
                     activeModal={activeModal}
                     currentImgId={currentImgId}
                     currentImgUrl={currentImgUrl}
+                    currentImgPosition={currentImgPosition}
+                    setCurrentImgPostion={setCurrentImgPostion}
+                    setCurrentImgUrl={setCurrentImgUrl}
                 />
                 <div className="grid lg:grid-cols-3 gap-5 pb-10">
-                    {data.galleries.map(el => {
+                    {data.galleries.map((el, index) => {
                         return (
                             <div 
                                 className="ml-5 mr-5 lg:m-0" 
                                 key={el.content.id} 
-                                onClick={()=>{activeModalHandler(el.content.id,el.content.publicUrl)}}
+                                onClick={()=>{activeModalHandler(el.content.id,el.content.publicUrl,index)}}
                             >
                                 <img src={el.content.publicUrl} alt='none'/>
                             </div>
